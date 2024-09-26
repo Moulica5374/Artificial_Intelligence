5a38cf0c560000dd76375204aa216094b5311f58
Moulica5374 <81550862+Moulica5374@users.noreply.github.com>
2024-09-26 16:31:19 -0500

    Delete Project2/lab2_572_Gomoku game final 2 directory

--- Project2/lab2_572_Gomoku game final 2/lab2_572_Gomoku game final 2/Source code/Player.java
+++
@@ -1,9 +0,0 @@
-public abstract class Player {
-    protected char playerChar;
-
-    public Player(char playerChar) {
-        this.playerChar = playerChar;
-    }
-
-    abstract boolean makeMove(GameBoard board);
-}
